971b88f41ceecd6d9f73ca8ed2965e035d4bdfaf
Roman <romanstolpovskij556@gmail.com>
2024-02-27 20:26:49 +0500

    +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
 <div class="nav-links">
 
+    {#each nav as link }
+    <a href={link.href} class="link">{link.title}</a>
+    {/each}
+
     {#if link.isShow}
 
     <a href={link.href} class="link">{link.title}</a>
