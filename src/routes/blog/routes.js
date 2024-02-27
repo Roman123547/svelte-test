d2d6c3471e1bbb9e96f04ad45e48a13993a27f69
Roman <romanstolpovskij556@gmail.com>
2024-02-27 20:15:10 +0500

    add blog

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -9,5 +9,15 @@ export const routes = [
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
     
     },
+
+    {
+
+        "id": 2,
+        
+        "title": "My Second Blog Post",
+        
+        "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+        
+        }
     
     ];
