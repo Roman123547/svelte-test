b80e5f864744e1f640930a3c8c0b66ec0962991d
Roman <romanstolpovskij556@gmail.com>
2024-02-27 19:17:30 +0500

    +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
 <div class="nav-links">
 
-    {#each nav as link}
+    {#if link,isShow}
         <a href={link.href} class="link">{link.title}</a>
-    {/each}
+    {/if}
 
 </div>
 </div>
@@ -21,23 +21,28 @@
     const nav = [
         {
             title:'Обо мне',
-            href: '/about'
+            href: '/about',
+            isShow: true,
         },
         {
             title:'Блог',
-            href: '/blog'
+            href: '/blog',
+            isShow: true,
         },
         {
             title:'Портфолио',
-            href: '/project'
+            href: '/project',
+            isShow: true,
         },
         {
             title:'Контакты',
-            href: '/contact'
+            href: '/contact',
+            isShow: true,
         },
         {
             title:'temp',
-            href: '/temp'
+            href: '/temp',
+            isShow: false,
         },
     ]
 </script>
